c8b0fc6f6b5ac0e9b66e21f9e81ea82c556e2010
Thomas DiGregorio <digregoriothomas@yahoo.com>
2019-09-11 12:58:45 -0700

    added external website to scraper class

--- lib/movie_gem/scraper.rb
+++ lib/movie_gem/scraper.rb
@@ -4,6 +4,14 @@ require 'pry'
 
 module MovieGem
     class Scraper
+        def self.get_site  
+            html = open("https://editorial.rottentomatoes.com/guide/best-sci-fi-movies-of-all-time/4/")
+            doc = Nokogiri::HTML(html)
+            doc.css("div.row.countdown-item").each do |card|
+            binding.pry
+            end
+        end
     end
 
 end
+MovieGem::Scraper.get_site
