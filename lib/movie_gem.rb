038ea45006f014c22dd500622f681b4b72abcbfe
Thomas DiGregorio <digregoriothomas@yahoo.com>
2019-09-12 19:26:21 -0700

    added code to movie_gem.rb file

--- lib/movie_gem.rb
+++ lib/movie_gem.rb
@@ -1,6 +1,13 @@
-require "movie_gem/version"
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
+require 'json'
+require 'colorize'
+require_relative "./movie_gem/version"
+require_relative './movie_gem/scraper'
+require_relative "./movie_gem/CLI"
 
-module MovieGem
-  class Error < StandardError; end
+#module MovieGem
+  #class Error < StandardError; end
   # Your code goes here...
-end
+#end
